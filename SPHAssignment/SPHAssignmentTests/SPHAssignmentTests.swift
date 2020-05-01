3fb93ff1b74fb4dbca460513efcb88c9ec069ee1
Akanksha <>
2020-05-01 23:39:48 +0800

    Add the Initial folder structure and files

---
+++ SPHAssignment/SPHAssignmentTests/SPHAssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  SPHAssignmentTests.swift
+//  SPHAssignmentTests
+//
+//  Created by Akanksha Thakur on 1/5/20.
+//  Copyright © 2020 Akanksha Thakur. All rights reserved.
+//
+
+import XCTest
+@testable import SPHAssignment
+
+class SPHAssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
